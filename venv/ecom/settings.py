2a4235984ced63252e22d2a0cb5f92e9a36e2d2b
yousef_amen <amenyousef857@gmail.com>
2024-11-26 21:01:03 +0200

    make some change in the allowed hosts varible in setting.py

--- venv/ecom/settings.py
+++ venv/ecom/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = 'django-insecure-mf$hu5t$ba*qca2c24@kyd*_oqr5+2^5nyp*+p%l(9l_l_0=!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['y-a-e-commrece-production.up.railway.app','https://y-a-e-commrece-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://y-a-e-commrece-production.up.railway.app']
+ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = []
 
 # Application definition
 
